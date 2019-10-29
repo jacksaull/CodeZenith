83b4e02eef3bcf5d5b3cf09aa668328547521bc8
jacksaull <32143271+jacksaull@users.noreply.github.com>
2019-10-29 18:04:50 +0000

    Camera Tracking Added
    Camera now tracks the Player on the X plane with a smooth linear movement, using LateUpdate()

---
+++ CodeZenith - Submarine Fan Project/Assets/Scripts/CameraTrack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+    public GameObject player;
+    private float playerX;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        playerX = player.transform.position.x;
+        transform.position = new Vector3(playerX, transform.position.y, transform.position.z);
+    }
+}
