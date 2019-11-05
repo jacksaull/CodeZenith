d6157f97b1b9177e974c5c9d649ab7a48748e082
jacksaull <32143271+jacksaull@users.noreply.github.com>
2019-11-05 22:19:53 +0000

    Controller Tilt, Min Speed and Fog/Water/Skybox added
    - Arduino now uses Comms to send what state the controller tilt is at to send to Unity
    - A minimun speed is now added to the player to keep them moving
    - Skybox is updated using a free 3rd party asset, blue fog and unity water added to give a underwater atmosphere

---
+++ CodeZenith - Submarine Fan Project/Assets/Scripts/Comms.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+
+public class Comms : MonoBehaviour
+{
+    [SerializeField]
+    internal string readString;
+    private int portNum;
+
+        SerialPort stream;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initialise the serial port
+        for (int i = 0; i < 10; i++)
+        {
+            portNum = i;
+            stream = new SerialPort("COM" + portNum, 9600);
+
+            if (!stream.IsOpen)
+            {
+                try
+                {
+                    Debug.Log("Trying to open serial port: " + "COM" + i);
+                    stream.Open();
+                    Debug.Log("Serial Port Open");
+
+                    break;
+                }
+                catch (Exception ex)  // If serial port is not open send error message
+                {
+                    Debug.Log("Failed to open port. Error: " + ex);
+                }
+            }
+
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- CodeZenith - Submarine Fan Project/Assets/Scripts/PlayerMovement.cs
+++ CodeZenith - Submarine Fan Project/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using UnityEngine;
 
+
+
 public class PlayerMovement : MonoBehaviour
 {
     public GameObject Rotor;
@@ -18,26 +20,32 @@ public class PlayerMovement : MonoBehaviour
     private float rotorRate = 5f;
     private float currentRotorRotation;
     public InfoUI ScriptUI;
+    private Comms arduino; //Added
 
-    //SerialPort stream = new SerialPort(DEVICE NAME, 9600); 
     void Start()
     {
         go_rigidBody = this.gameObject.GetComponent<Rigidbody>();
         go_rigidBody.useGravity = false;
         Rotor.transform.localRotation = Quaternion.Euler(-180, 0, 0);
-
-        //stream.Open();
+        arduino = GetComponent<Comms>(); // added
     }
 
     void Update()
     {
-        //string controllerValue = stream.ReadLine();
         rotorSpeed = rotorRate * smagnitude;
         Rotor.transform.Rotate(0, 0, rotorSpeed, Space.Self);
     }
 
     private void FixedUpdate()
     {
+        go_rigidBody.AddForce(transform.forward * 2);
+        smagnitude = go_rigidBody.velocity.magnitude;
+
+        if (this.transform.position.y > 6.5)
+        {
+            this.transform.position = new Vector3(transform.position.x, 6.5f, transform.position.z);
+        }
+
         if (Input.GetMouseButton(0))
         {
             if (currentRotation == -30)
@@ -67,14 +75,60 @@ public class PlayerMovement : MonoBehaviour
                 rotation = Quaternion.Euler(rotationX, 90, 180);
                 this.gameObject.transform.rotation = rotation;
             }
-
-            if (this.transform.rotation.x >= 30)
-            {
-                rotation = Quaternion.Euler(30, 90, 180);
-                this.gameObject.transform.rotation = rotation;
-            }
         }
 
+
+        //if (arduino.readString == "Submarine going up")
+        //{
+        //    if (currentRotation == -30)
+        //    {
+        //        rotation = Quaternion.Euler(-30, 90, 180);
+        //        this.gameObject.transform.rotation = rotation;
+        //    }
+        //    else
+        //    {
+        //        rotationX -= 0.5f;
+        //        currentRotation -= 0.5f;
+        //        rotation = Quaternion.Euler(rotationX, 90, 180);
+        //        this.gameObject.transform.rotation = rotation;
+        //    }
+        //}
+        //else if (arduino.readString == "Submarine going down")
+        //{
+        //    if (currentRotation == 30)
+        //    {
+        //        rotation = Quaternion.Euler(30, 90, 180);
+        //        this.gameObject.transform.rotation = rotation;
+        //    }
+        //    else
+        //    {
+        //        rotationX += 0.5f;
+        //        currentRotation += 0.5f;
+        //        rotation = Quaternion.Euler(rotationX, 90, 180);
+        //        this.gameObject.transform.rotation = rotation;
+        //    }
+        //}
+        //else if (arduino.readString == "Submarine is level")
+        //{
+        //  if (currentRotation < 0)
+        //  {
+        //      rotationX += 0.5f;
+        //      currentRotation += 0.5f;
+        //      rotation = Quaternion.Euler(rotationX, 90, 180);
+        //      this.gameObject.transform.rotation = rotation;
+        //  }
+        //  else if (currentRotation > 0)
+        //  {
+        //      rotationX -= 0.5f;
+        //      currentRotation -= 0.5f;
+        //      rotation = Quaternion.Euler(rotationX, 90, 180);
+        //      this.gameObject.transform.rotation = rotation;
+        //  }
+        //}
+
+        /*------------------------------------------------------------
+         * ----------------------------------------------------------
+         */
         if (Input.GetKey("w"))
         {
             go_rigidBody.AddForce(transform.forward * speed);
